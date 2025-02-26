594a6ae1e30b1f4920cce9ec12b1a639ac2a4375
Dominik <55330408+Dom-in@users.noreply.github.com>
2025-02-26 15:50:35 +0000

    Create players.sql

---
+++ players.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `players` (
+  `ID` int NOT NULL,
+  `RockstarID` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `money` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '{"bank":100,"cash":100,"black_money":0}',
+  `position` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '{"x":-222.98,"y":430.16,"z":14.82,"h":122.23}'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE `players`
+  ADD PRIMARY KEY (`ID`);
+
+ALTER TABLE `players`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
